dca8dcafcb06813ec699e3b107faa0d776e1aeff
Tina Giska <tina.tylicka@gmail.com>
2018-04-08 21:21:15 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
-$(function changeSlide(){
-  var carouselList = $('#carousel ul');
-  setTimeout(changeSlide,3000);
-  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 
-  function moveFirstSlide(){
-  var firstItem = carouselList.find('li:first');
-  var lastItem = carouselList.find('li:last');
-  lastItem.after(firstItem);
-  carouselList.css({marginLeft:0});
-};
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+		var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+		if (tweetText.length > 140) {
+    getQuote();
+		} else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+		$.getJSON(prefix + quoteUrl, createTweet);
+		$.ajaxSetup({ cache: false });
+
+		}
+
+}
+
+		$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+    getQuote();
+    })
 });
